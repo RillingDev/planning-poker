d5e9d240fa6044ff6c9373d12ccc418a77fb74ef
Felix Rilling <felix.rilling@cryptshare.com>
2022-11-17 09:22:08 +0100

    Initialized project.

---
+++ src/main/java/com/cryptshare/planningpoker/PlanungspokertoolApplication.java
@@ -0,0 +1,13 @@
+package com.cryptshare.planningpoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanungspokertoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanungspokertoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cryptshare/planningpoker/PlanungspokertoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptshare.planningpoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanungspokertoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
