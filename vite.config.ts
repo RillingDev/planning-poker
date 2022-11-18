6d65aea7554e360ba0fb171e1c7d2b215168ff89
Felix Rilling <felix.rilling@cryptshare.com>
2022-11-18 13:01:42 +0100

    PPT-3: Added vite.

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react()],
+	build: {
+		outDir: "src/main/resources/static/"
+	},
+	server: {
+		host: "127.0.0.1"
+	}
+});
