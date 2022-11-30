52ebe505f69417fd5a646f020778d1baaa0ebab3
Felix Rilling <felix.rilling@cryptshare.com>
2022-11-30 13:53:28 +0100

    PPT-9: Unify modal handling.

--- src/main/react/routes/RoomListView.tsx
+++ src/main/react/routes/RoomListView.tsx
@@ -22,19 +22,16 @@ export const RoomListView: FC = () => {
 	const [error, handleError, resetError] = useErrorHandler();
 
 	const loaderData = useLoaderData() as LoaderResult;
-
 	const [rooms, setRooms] = useState<Room[]>(loaderData.rooms);
 
-	const [modalVisible, setModalVisible] = useState(false);
-	const showModal = () => setModalVisible(true);
-	const hideModal = () => setModalVisible(false);
+	const [creationModalVisible, setCreationModalVisible] = useState(false);
 
 	const updateRooms = async () => {
 		setRooms(await loadRooms());
 	};
 
 	const handleCreationSubmit = (newRoomName: string, newRoomCardSet: CardSet) => {
-		hideModal();
+		setCreationModalVisible(false);
 		createRoom(newRoomName, newRoomCardSet.name)
 			.then(updateRooms)
 			.catch(handleError);
@@ -54,8 +51,8 @@ export const RoomListView: FC = () => {
 
 			<header className="room-list__header">
 				<h2>Rooms</h2>
-				<Button variant="primary" onClick={showModal}>Create Room</Button>
-				<CreateRoomModal onSubmit={handleCreationSubmit} show={modalVisible} onHide={hideModal}></CreateRoomModal>
+				<Button variant="primary" onClick={() => setCreationModalVisible(true)}>Create Room</Button>
+				<CreateRoomModal onSubmit={handleCreationSubmit} show={creationModalVisible} onHide={() => setCreationModalVisible(false)}></CreateRoomModal>
 			</header>
 			<nav>
 				<ul className="room-list">
