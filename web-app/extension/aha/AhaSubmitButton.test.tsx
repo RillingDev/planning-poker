a0a0f83722f6fdbd8021b1e7b65fb7dc4ea01a1a
Felix Rilling <felix.rilling@cryptshare.com>
2023-03-22 16:54:00 +0100

    Tests for AhaSubmitButton.

--- web-app/extension/aha/AhaSubmitButton.test.tsx
+++ web-app/extension/aha/AhaSubmitButton.test.tsx
@@ -1,12 +1,21 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { MockedObject, vi } from "vitest";
+import { getExtensionRoomConfig } from "../../api";
 import { createRoom, createVoteSummary } from "../../test/dataFactory";
 import { ahaExtension } from "./AhaExtension";
 import { AhaSubmitButton } from "./AhaSubmitButton";
 import type { AhaClient } from "./api";
+import { AhaRoomConfig } from "./api";
 
 vi.mock("./AhaExtension");
+
+vi.mock("../../api", () => {
+	return {
+		getExtensionRoomConfig: vi.fn(),
+		editExtensionRoomConfig: vi.fn(),
+	};
+});
 
 describe("AhaSubmissionModal", () => {
 
@@ -18,7 +27,7 @@ describe("AhaSubmissionModal", () => {
 	it("shows button", () => {
 		render(<AhaSubmitButton
 			room={createRoom({topic: "ABC-I-123"})}
-			voteSummary={createVoteSummary({average: 10.5})}
+			voteSummary={createVoteSummary({average: 10.9})}
 		/>);
 
 		expect(screen.getByText("Save to Aha!")).toBeInTheDocument();
@@ -30,7 +39,7 @@ describe("AhaSubmissionModal", () => {
 	it("hides button if topic is not in the right syntax", () => {
 		render(<AhaSubmitButton
 			room={createRoom({topic: "X"})}
-			voteSummary={createVoteSummary({average: 10.5})}
+			voteSummary={createVoteSummary({average: 10.9})}
 		/>);
 
 		expect(screen.getByText("Save to Aha!")).not.toBeVisible();
@@ -39,11 +48,61 @@ describe("AhaSubmissionModal", () => {
 	it("shows modal", async () => {
 		render(<AhaSubmitButton
 			room={createRoom({topic: "ABC-I-123"})}
-			voteSummary={createVoteSummary({average: 10.5})}
+			voteSummary={createVoteSummary({average: 10.9})}
 		/>);
 
 		await userEvent.click(screen.getByText("Save to Aha!"));
 
 		expect(screen.getByText("Submit")).toBeInTheDocument();
 	});
+
+
+	it("shows error when idea loading fails", async () => {
+		vi.mocked(ahaClient.getIdea<"name" | "reference_num">).mockResolvedValue(null);
+
+		render(<AhaSubmitButton
+			room={createRoom({topic: "ABC-I-123"})}
+			voteSummary={createVoteSummary({average: 10.9})}
+		/>);
+
+		await userEvent.click(screen.getByText("Save to Aha!"));
+
+		await waitFor(() => expect(screen.getByText("Loading Idea")).not.toBeVisible());
+
+		expect(screen.getByText("Could not find idea 'ABC-I-123'.")).toBeInTheDocument();
+		expect(screen.getByText("Submit")).toBeDisabled();
+	});
+
+	it("loads idea", async () => {
+		vi.mocked(ahaClient.getIdea<"name" | "reference_num">).mockResolvedValue({
+			idea: {
+				id: "123", product_id: "456", reference_num: "ABC-I-123", name: "Foo"
+			}
+		});
+		vi.mocked(ahaClient.getIdeasForProduct<"score_facts">).mockResolvedValue({
+			ideas: [],
+			pagination: {
+				total_records: 0,
+				total_pages: 0,
+				current_page: 1,
+			}
+		});
+		vi.mocked(getExtensionRoomConfig<AhaRoomConfig>).mockResolvedValue({scoreFactName: null});
+
+		render(<AhaSubmitButton
+			room={createRoom({topic: "ABC-I-123"})}
+			voteSummary={createVoteSummary({average: 10.9})}
+		/>);
+
+		await userEvent.click(screen.getByText("Save to Aha!"));
+
+		await waitFor(() => expect(screen.getByText("Loading Idea")).not.toBeVisible());
+
+		expect(screen.getByText("ABC-I-123: Foo")).toBeInTheDocument();
+		// Rounded score
+		expect(screen.getAllByDisplayValue("11")).toBeInTheDocument();
+		expect(screen.getByText("Submit")).toBeEnabled();
+	});
+
+
 });
