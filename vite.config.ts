a178c5a72415b6e79ee198de64183a8d8c010443
Felix Rilling <felix.rilling@cryptshare.com>
2023-03-24 10:31:13 +0100

    Fixed coverage reporting.

--- vite.config.ts
+++ vite.config.ts
@@ -27,6 +27,7 @@ export default defineConfig(({command, mode}) => {
 			include: ["./web-app/**/*.test.{ts,tsx}"],
 			coverage: {
 				provider: "c8",
+				reporter: ["lcovonly"],
 				reportsDirectory: "./.local/coverage"
 			}
 		}
