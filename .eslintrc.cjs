a828371c1ad06e061195d5b2316e4564411fed64
Felix Rilling <felix.rilling@cryptshare.com>
2023-04-21 09:20:54 +0200

    Updated eslint config based on new defaults from vite.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,34 +1,33 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
-
 module.exports = {
-    root: true,
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint", "prettier"],
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-        "plugin:jsx-a11y/recommended",
-        "plugin:react/recommended",
-        "plugin:react/jsx-runtime",
-        "plugin:react-hooks/recommended",
-        "prettier",
-    ],
-    parserOptions: {
-        tsconfigRootDir: __dirname,
-        project: ["./tsconfig.json", "./tsconfig.node.json"],
+  root: true,
+  env: { browser: true },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  settings: { react: { version: "detect" } },
+  rules: {
+    "@typescript-eslint/no-non-null-assertion": "off",
+  },
+  overrides: [
+    {
+      files: ["**/*.test.ts?(x)"],
+      rules: {
+        "@typescript-eslint/unbound-method": "off",
+      },
     },
-    settings: {react: {version: "detect"}},
-    rules: {
-        "@typescript-eslint/no-non-null-assertion": "off",
-    },
-    overrides: [
-        {
-            files: ["**/*.test.ts?(x)"],
-            rules: {
-                "@typescript-eslint/unbound-method": "off",
-            },
-        },
-    ],
+  ],
 };
