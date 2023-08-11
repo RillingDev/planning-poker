c07c469eb4931d5ca6ccc3ba352f114e6b4286a7
Felix Rilling <felix.rilling@cryptshare.com>
2023-08-11 11:33:05 +0200

    Extract context creation.

--- web-app/AppContext.ts
+++ web-app/AppContext.ts
@@ -1,6 +1,8 @@
 import { createContext } from "react";
 import { ExtensionManager } from "./extension/ExtensionManager";
 import { CardSet, User } from "./model";
+import { getCardSets, getExtensions, getIdentity } from "./api.ts";
+import { AVAILABLE_EXTENSIONS } from "./extension/extensions.ts";
 
 export interface AppContextState {
   user: User;
@@ -13,3 +15,23 @@ export const AppContext = createContext<AppContextState>({
   cardSets: [],
   extensionManager: new ExtensionManager([]),
 });
+
+export async function createContextState(): Promise<AppContextState> {
+  const [user, enabledExtensionKeys, cardSets] = await Promise.all([
+    getIdentity(),
+    getExtensions(),
+    getCardSets(),
+  ]);
+
+  const extensionManager = new ExtensionManager(
+    AVAILABLE_EXTENSIONS.filter((availableExtension) =>
+      enabledExtensionKeys.includes(availableExtension.key),
+    ),
+  );
+
+  return {
+    cardSets,
+    user,
+    extensionManager,
+  };
+}
